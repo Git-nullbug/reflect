c1839ac16003e35f21ad55e2765cd2afdad0b09b
732633667 <732633667@qq.com>
2019-12-19 11:54:24 +0800

    优化了创建无参对象和有参对象的逻辑，优化了字符串的拼接。

--- src/main/java/cn/xdl/util/Parse.java
+++ src/main/java/cn/xdl/util/Parse.java
@@ -24,16 +24,16 @@ public class Parse {
 
     //private Map<String,Object> map = null;
     public List<Bean> parseXML() {
-        InputStream is = null;
+        InputStream is = null;          //创建指向XML文档的输入流
         try {
             is = new FileInputStream("F:/Java34/0- Java-tools/IDEA/idea-work/reflect/src/main/resources/bean.xml");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        SAXReader sr = new SAXReader();
+        SAXReader sr = new SAXReader();         //创建SAXReader类对象 XM了读取工具对象
         Document doc = null;
         try {
-            doc = sr.read(is);
+            doc = sr.read(is);                  //通过read方法传入文档流得到Document对象
         } catch (DocumentException e) {
             e.printStackTrace();
         }
@@ -60,61 +60,39 @@ public class Parse {
     }
 
     /* 创建对象 */
-    public Map<String,Object> createBean(){
+    public Map<String,Object> createBean() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
         List<Bean> beanList = parseXML();                   //得到解析后的参数集合
         Map<String,Object> map = new HashMap<>();           //创建一个Map集合将创建好的对象都存在该集合中
         for(Bean bean : beanList){                          //遍历List集合中的参数对象
             Map pros = bean.getProperties();
-            if(pros.isEmpty()){                             //如果参数bean对象中的参数成员变量为空，则直接反射创建无参对象
-                String id = bean.getId();                   //得到id属性值
-                String clazz = bean.getClazz();             //得到class属性值
-                try {
-                    Class c = Class.forName(clazz);         //得到Class类对象
-                    Object obj = c.newInstance();           //通过Class对象来创建实例对象
-                    map.put(id,obj);                        //将创建好的对象存储到Map集合中
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }else{                                          //当集合不为空时
-                String id = bean.getId();                   //得到id属性值
-                String clazz = bean.getClazz();             //得到class属性值
-                try {
-                    Class c = Class.forName(clazz);         //通过class的属性值得到Class对象
-                    Object obj = c.newInstance();           //再通过Class对象创建class属性值所对应的实例对象
-                    for(Object key : pros.keySet()){        //遍历参数
-                        Object value = pros.get(key);       //得到key对应的value值 key对应就是方法名 value对应的是参数值
-                        /* 字符串的转换开始 */
-                        String str = (String) key;
-                        String first = str.substring(0,1);
-                        String firstUpper = first.toUpperCase();
-                        StringBuilder sb = new StringBuilder(str);
-                        sb.replace(0,1,firstUpper);
-                        String methodName = "set"+sb;
-                        /* 字符串的转换结束 */
-                        String className = value.getClass().getName();
-                        Field field = c.getDeclaredField((String) key);
-                        //通过key拿到Class类中的成员属性名
-                        Method method = c.getMethod(methodName,field.getType());
-                        Class<?> type = field.getType();
-                        //field.setAccessible(true);
-                        if(value.getClass().getName().equals(type.getName())) {
-                            method.invoke(obj,value);
-                        }else {
-                            Integer integer = Integer.valueOf((String)value);
-                            method.invoke(obj,integer);
-                        }
+            String id = bean.getId();                   //得到id属性值
+            String clazz = bean.getClazz();             //得到class属性值
+            Class c = Class.forName(clazz);         //得到Class类对象
+            Object obj = c.newInstance();           //通过Class对象来创建实例对象
+            if(!pros.isEmpty()) {                             //如果参数bean对象中的参数成员变量为空，则直接反射创建无参对象
+                for (Object key : pros.keySet()) {        //遍历参数
+                    Object value = pros.get(key);       //得到key对应的value值 key对应就是方法名 value对应的是参数值
+                    String str = (String) key;
+                    String methodName = "set"+str.substring(0, 1).toUpperCase()+str.substring(1);
+                    String className = value.getClass().getName();
+                    Field field = c.getDeclaredField((String) key);
+                    //通过key拿到Class类中的成员属性名
+                    Method method = c.getMethod(methodName, field.getType());
+                    Class<?> type = field.getType();
+                    if (value.getClass().getName().equals(type.getName())) {
+                        method.invoke(obj, value);
+                    } else {
+                        Integer integer = Integer.valueOf((String) value);
+                        method.invoke(obj, integer);
                     }
-                    map.put(id,obj);
-                } catch (ClassNotFoundException | NoSuchFieldException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                    e.printStackTrace();
                 }
-
+                map.put(id, obj);
             }
         }
         return map;
     }
 
-    public Object getBean(String id){
+    public Object getBean(String id) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException, NoSuchMethodException, ClassNotFoundException {
         Map<String, Object> beans = createBean();
         for(Object beanId : beans.keySet()){
             if(id.equals(beanId)){
